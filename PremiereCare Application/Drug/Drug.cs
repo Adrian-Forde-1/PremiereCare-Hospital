7d8e05c1ac54ab71b820bc105cf12112f5515f34
Adrian Forde <AdrianForde02@gmail.com>
2020-10-31 03:01:39 -0400

    Added a Drug folder and class

---
+++ PremiereCare Application/Drug/Drug.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PremiereCare_Application.Drug
+{
+    class Drug
+    {
+
+        public string name { get; set; }
+        public string cost { get; set; }
+    }
+}
